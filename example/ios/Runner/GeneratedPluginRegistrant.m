2af315e145747c62aaf3ba24870f364563bf60f7
AhmedAbouelkher <ahmedabouelkher1@gmail.com>
2022-10-04 01:54:33 +0200

    initial

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_zoom_sdk/FlutterZoomSdkPlugin.h>)
+#import <flutter_zoom_sdk/FlutterZoomSdkPlugin.h>
+#else
+@import flutter_zoom_sdk;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterZoomSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterZoomSdkPlugin"]];
+}
+
+@end
